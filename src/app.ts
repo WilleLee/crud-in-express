12137829d0e5c2c118ace4f26f7cf86b50722379
Wille Lee <1992season@gmail.com>
2022-08-14 19:55:01 +0900

    add nodemon, post apiRouter, src folder

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import apiRouter from "./routes/apiRouter";
 const PORT = 4000;
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get("/", (req, res) => {
   return res.send("hello world");
 });
--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -7,6 +7,19 @@ apiRouter.get("/", (req, res) => {
   return res.send("this is apiRouter");
 });
 
+apiRouter.post("/posts", (req, res) => {
+  try {
+    const id = Date.now();
+    const { title, text } = req.body;
+    const postToPost = { id, title, text };
+    posts.push(postToPost);
+    return res.status(200).send("successfully posted");
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("unable to post");
+  }
+});
+
 apiRouter.get("/posts/:id", (req, res) => {
   const { id } = req.params;
   const postToGet = posts.find((post) => String(post.id) === id);
