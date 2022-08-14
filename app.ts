23e826b5ac90d5829eca81028eded412a3c2a4f9
Wille Lee <1992season@gmail.com>
2022-08-14 18:26:14 +0900

    add api router and ust it in app.ts

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+//routes
+import apiRouter from "./routes/apiRouter";
 
 const PORT = 4000;
 const app = express();
@@ -6,6 +8,8 @@ const app = express();
 app.get("/", (req, res) => {
   return res.send("hello world");
 });
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to the port ${PORT}`);
