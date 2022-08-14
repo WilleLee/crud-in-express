98aff5b3b65d1cf0090739212fb871e059bb8706
Wille Lee <1992season@gmail.com>
2022-08-14 19:34:45 +0900

    add get method

--- routes/apiRouter.ts
+++ routes/apiRouter.ts
@@ -7,4 +7,10 @@ apiRouter.get("/", (req, res) => {
   return res.send("this is apiRouter");
 });
 
+apiRouter.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const postToGet = posts.find((post) => String(post.id) === id);
+  return res.send(postToGet);
+});
+
 export default apiRouter;
